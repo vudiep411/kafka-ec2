faecd72d716d7e9cea6f9c102f593a82890a63b3
vudiep411 <vudiep411@gmail.com>
2023-08-20 22:26:00 -0700

    tear down

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y default-jre
+sudo yum update -y
+sudo yum install -y java-1.8.0-openjdk-devel
   # Download and install Kafka (replace the URL with the actual Kafka download link)
 wget https://archive.apache.org/dist/kafka/3.5.1/kafka_2.12-3.5.1.tgz
 tar -xzf kafka_2.12-3.5.1.tgz
